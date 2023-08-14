2c7f8307f4f16612b2bd1ac39661329fa38da9ab
Gideon Addo <gideonaddoofori@gmail.com>
2023-08-14 19:58:54 +0000

    Starts with '$' when invoked

--- loop_shell.c
+++ loop_shell.c
@@ -37,7 +37,7 @@ char *no_comment(char *in)
 
 /**
  * loop_shell - Loop of shell
- * @data: data relevant (av, input, args)
+ * @data: data of type shell_data relevant (av, input, args)
  *
  * Return: nothing.
  */
@@ -49,7 +49,7 @@ void loop_shell(shell_data *data)
 	loop = 1;
 	while (loop == 1)
 	{
-		write(STDIN_FILENO, "^-^ ", 4);
+		write(STDIN_FILENO, "$ ", 4);
 		input = read_input(&i);
 		if (i != -1)
 		{
